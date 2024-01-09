9b5ac7bdfeb475a0faf67b23ccc609784c298032
Joose Sainio <joose.sainio@tuni.fi>
2024-01-09 09:39:04 +0200

    Fix performance + add all stats

--- main.cpp
+++ main.cpp
@@ -10,6 +10,19 @@ struct sub_image_stats {
     uint16_t y;
     uint8_t width;
     uint8_t height;
+
+    uint32_t split_tree;
+
+    uint8_t qp;
+
+    uint8_t intra_mode;
+    uint8_t is_mip;
+    uint8_t mip_transpose;
+    uint8_t mrl;
+    uint8_t isp;
+
+    uint8_t lfnst;
+    uint8_t tr_idx;
 } __attribute__((packed));
 
 struct sub_image {
@@ -18,6 +31,7 @@ struct sub_image {
     cv::Mat image;
 };
 
+
 sub_image readOneCU(std::ifstream &data_file) {
     sub_image cu;
     // Skip type and timestamp
@@ -25,8 +39,7 @@ sub_image readOneCU(std::ifstream &data_file) {
     data_file.read(temp_buffer, 1);
     // data_file.seekg(9, std::ios::cur);
     data_file.read(reinterpret_cast<char *>(&cu.stats), sizeof(cu.stats));
-    // Skip the data
-    data_file.seekg(9, std::ios::cur);
+
     if (data_file.eof() || !data_file.good()) {
         cu.stats.width = 0;
         cu.stats.height = 0;
@@ -35,7 +48,7 @@ sub_image readOneCU(std::ifstream &data_file) {
     cu.rect = cv::Rect(cu.stats.x, cu.stats.y, cu.stats.width, cu.stats.height);
     uint8_t *yuv420 = new uint8_t[cu.stats.width * cu.stats.height * 3 / 2];
     data_file.read(reinterpret_cast<char *>(yuv420), cu.stats.width * cu.stats.height * 3 / 2);
-    cv::Mat yuvMat(cu.stats.height + cu.stats.height / 2, cu.stats.width, CV_8UC1, const_cast<unsigned char*>(yuv420));
+    cv::Mat yuvMat(cu.stats.height + cu.stats.height / 2, cu.stats.width, CV_8UC1, const_cast<unsigned char *>(yuv420));
     cu.image = cv::Mat::zeros(cu.stats.height, cu.stats.width, CV_8UC3);
     cv::cvtColor(yuvMat, cu.image, cv::COLOR_YUV2RGBA_I420);
     delete[] yuv420;
@@ -43,7 +56,6 @@ sub_image readOneCU(std::ifstream &data_file) {
 }
 
 int main() {
-    // RBG -> BGR
     static const sf::Color colors[4] = {
             sf::Color(245, 24, 245),
             sf::Color(255, 255, 5),
@@ -77,7 +89,7 @@ int main() {
     bool running = true;
     bool show_grid = true;
     while (running) {
-        if(data_file.eof() || !data_file.good()) {
+        if (data_file.eof() || !data_file.good()) {
             break;
         }
         // Read one CU from the data file
@@ -85,9 +97,12 @@ int main() {
         sf::RenderTexture cuEdgeRenderTexture;
         sf::Image image = cuEdgeTexture.copyToImage();
         image.flipVertically();
+
+        sf::Image newImage;
+        newImage.create(1280, 720, sf::Color::Transparent);
         while (current_cu.stats.timestamp - timestamp < 33'000'000) {
             current_cu = readOneCU(data_file);
-            if (data_file.eof() || !data_file.good() || current_cu.stats.width == 0 || current_cu.stats.height == 0 ) {
+            if (data_file.eof() || !data_file.good() || current_cu.stats.width == 0 || current_cu.stats.height == 0) {
                 break;
             }
             temp_timestamp = current_cu.stats.timestamp;
@@ -95,30 +110,32 @@ int main() {
             sf::Image cuImage;
             cuImage.create(current_cu.stats.width, current_cu.stats.height, current_cu.image.data);
 
-            // Copy the CU into the RenderTexture
-            sf::Texture cuTexture;
-            cuTexture.loadFromImage(cuImage);
-            sf::Sprite cuSprite(cuTexture);
-            cuSprite.setPosition(current_cu.rect.x, current_cu.rect.y);
-            imageTexture.draw(cuSprite);
+            newImage.copy(cuImage, current_cu.stats.x, current_cu.stats.y);
 
-            for(int y = current_cu.rect.y; y < current_cu.rect.y + current_cu.rect.height - 1; y++) {
-                for(int x = current_cu.rect.x; x < current_cu.rect.x + current_cu.rect.width - 1; x++) {
+            for (int y = current_cu.rect.y; y < current_cu.rect.y + current_cu.rect.height - 1; y++) {
+                for (int x = current_cu.rect.x; x < current_cu.rect.x + current_cu.rect.width - 1; x++) {
                     image.setPixel(x, y, sf::Color::Transparent);
                 }
             }
 
             // Draw the lines to the image
             for (int x = current_cu.rect.x + 1; x < current_cu.rect.x + current_cu.rect.width; x++) {
-                image.setPixel(x - 1, current_cu.rect.y + current_cu.rect.height - 1, colors[current_cu.stats.frame_num % 4]);
+                image.setPixel(x - 1, current_cu.rect.y + current_cu.rect.height - 1,
+                               colors[current_cu.stats.frame_num % 4]);
             }
             for (int y = current_cu.rect.y; y < current_cu.rect.y + current_cu.rect.height; y++) {
-                image.setPixel(current_cu.rect.x + current_cu.rect.width - 1, y, colors[current_cu.stats.frame_num % 4]);
+                image.setPixel(current_cu.rect.x + current_cu.rect.width - 1, y,
+                               colors[current_cu.stats.frame_num % 4]);
             }
 
         }
         image.flipVertically();
         cuEdgeTexture.update(image);
+
+        sf::Texture newTexture;
+        newTexture.loadFromImage(newImage);
+        sf::Sprite newSprite(newTexture);
+        imageTexture.draw(newSprite);
 
         {
             cuEdgeRenderTexture.create(1280, 720);
@@ -126,14 +143,21 @@ int main() {
             cuEdgeRenderTexture.draw(sprite);
         }
         timestamp = temp_timestamp;
+        // Get the position of the cursor relative to the window
+        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+
+
 
         // Get the current size of the window
         sf::Vector2u windowSize = window.getSize();
 
         // Calculate the scale factors for the sprite
-        float scaleX = fullscreen ? 2 : imageTexture.getSize().x / static_cast<float>(windowSize.x);
-        float scaleY = fullscreen ? 2 : imageTexture.getSize().y / static_cast<float>(windowSize.y);
+        float scaleX = fullscreen ? 2 : sqrt(static_cast<float>(windowSize.x) / imageTexture.getSize().x);
+        float scaleY = fullscreen ? 2 : sqrt(static_cast<float>(windowSize.y) / imageTexture.getSize().y);
 
+//        std::cout << "Mouse X: " << mousePosition.x << ", Y: " << mousePosition.y << std::endl;
+//        std::cout << imageTexture.getSize().x << ", " << imageTexture.getSize().y << std::endl;
+//        std::cout << windowSize.x << ", " << windowSize.y << std::endl;
 
         // Display the frame
         imageTexture.display();
@@ -144,7 +168,9 @@ int main() {
 
         window.draw(sprite);
         if (show_grid) {
-            window.draw(sf::Sprite(cuEdgeRenderTexture.getTexture()));
+            sf::Sprite grid_sprite = sf::Sprite(cuEdgeRenderTexture.getTexture());
+            grid_sprite.setScale(scaleX, scaleY);
+            window.draw(grid_sprite);
         }
         window.display();
 
